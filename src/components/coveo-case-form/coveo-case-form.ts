2b2dd37ffcf07f183d9c4709fd2f3bc8a138b7e2
Pixel <louis.vincent.bompart@gmail.com>
2020-04-15 13:51:35 -0400

    Perfect follow

--- src/components/coveo-case-form/coveo-case-form.ts
+++ src/components/coveo-case-form/coveo-case-form.ts
@@ -17,6 +17,7 @@ export class CoveoCaseForm extends LitElement {
   static canvas: HTMLCanvasElement;
   static CSSOffset: { x: number; y: number } = { x: 16, y: 8 };
   popup: HTMLElement;
+  observer: MutationObserver;
   static get styles() {
     return css`
       div {
@@ -57,37 +58,86 @@ export class CoveoCaseForm extends LitElement {
     const foo: TextArea = this.shadowRoot.getElementById("foo") as TextArea;
     const evtListener = (e: Event) => {
       const target = e.target;
-      setTimeout(() => {
-        if (target instanceof TextArea) {
-          const positions = getCaretCoordinates(
-            target.shadowRoot.activeElement as TextArea,
-            target.selectionEnd,
-            this.shadowRoot.firstElementChild,
-            { debug: false }
-          );
-          if (!this.popup) {
-            this.popup = document.createElement("div");
-            this.popup.style.backgroundColor = "red";
-            this.popup.style.position = "absolute";
-            this.popup.style.zIndex = "2";
-            this.shadowRoot.firstElementChild.append(this.popup);
-            this.popup.style.marginRight = "0";
-            this.popup.style.marginTop = "0";
-            this.popup.style.marginBottom = "0";
-            this.popup.style.marginLeft = "0";
-            this.popup.style.width = "0";
-            this.popup.style.height = "0";
-          }
-          this.popup.style.width = positions.fontSize + "px";
-          this.popup.style.height = this.popup.style.width;
-          this.popup.style.marginLeft = positions.fontSize * 0 + "px";
-          this.popup.style.top = positions.top + "px";
-          this.popup.style.left = positions.left + "px";
-        }
-      }, 50);
+      this.positionPopup(target);
     };
     foo.addEventListener("input", evtListener);
-    // foo.addEventListener("wheel", evtListener);
-    // foo.addEventListener("keypress", evtListener);
+    this.observer = new MutationObserver((mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "attributes") {
+          if (mutation.attributeName === "scrollTop") {
+            this.positionPopup(foo);
+          }
+        }
+      }
+    });
+    const domLoadObserve = new MutationObserver((mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+          const textarea = this.getNodeContainingArea(
+            Array.from(mutation.addedNodes).find((node) => {
+              if (node instanceof Element) {
+                return Boolean(this.getNodeContainingArea(node));
+              }
+            }) as Element
+          );
+
+          if (textarea) {
+            function debounce(func, wait, immediate?) {
+              var timeout;
+              return function () {
+                var context = this,
+                  args = arguments;
+                clearTimeout(timeout);
+                timeout = setTimeout(function () {
+                  timeout = null;
+                  if (!immediate) func.apply(context, args);
+                }, wait);
+                if (immediate && !timeout) func.apply(context, args);
+              };
+            }
+            textarea.addEventListener("scroll", (event) => {
+              debounce(this.positionPopup(foo), 10);
+            });
+          }
+        }
+      }
+    });
+    domLoadObserve.observe(foo.shadowRoot, { childList: true, subtree: true });
+  }
+
+  getNodeContainingArea(node: Element): Element {
+    if (node instanceof HTMLTextAreaElement) {
+      return node;
+    }
+    return node.querySelector("textarea");
+  }
+
+  private positionPopup(target: EventTarget) {
+    if (target instanceof TextArea) {
+      const positions = getCaretCoordinates(
+        target.shadowRoot.activeElement as TextArea,
+        target.selectionEnd,
+        this.shadowRoot.firstElementChild,
+        { debug: false }
+      );
+      if (!this.popup) {
+        this.popup = document.createElement("div");
+        this.popup.style.backgroundColor = "red";
+        this.popup.style.position = "absolute";
+        this.popup.style.zIndex = "2";
+        this.shadowRoot.firstElementChild.append(this.popup);
+        this.popup.style.marginRight = "0";
+        this.popup.style.marginTop = "0";
+        this.popup.style.marginBottom = "0";
+        this.popup.style.marginLeft = "0";
+        this.popup.style.width = "0";
+        this.popup.style.height = "0";
+      }
+      this.popup.style.width = positions.fontSize + "px";
+      this.popup.style.height = this.popup.style.width;
+      this.popup.style.marginLeft = positions.fontSize * 0.2 + "px";
+      this.popup.style.top = positions.top + "px";
+      this.popup.style.left = positions.left + "px";
+    }
   }
 }
